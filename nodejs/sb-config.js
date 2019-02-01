59c44a31ec9bce4be17f1ad7995ac1868cb3d1fb
snippetbucket <snippetbucket@gmail.com>
2019-02-01 11:47:46 +0530

    boing.com, confluence with nodejs with external file

---
+++ nodejs/sb-config.js
@@ -0,0 +1,17 @@
+/***
+
+    Author: Tejas Tank
+    Powered By: SnippetBucket Technoliges, Offshore Office.
+    Email: info@snippetbucket.com
+
+*/
+
+module.exports = {
+    config: function(){
+        return {
+            'host': '192.168.1.119',
+            'user': 'test1',
+            'password': 'sb@123',
+          };
+    }
+}
